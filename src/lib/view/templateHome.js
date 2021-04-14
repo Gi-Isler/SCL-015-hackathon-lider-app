296f664cdf3c2a9b0a534c4beced84071a85e1d3
Saya Yaczms <marce.catalan.castelli@gmail.com>
2021-04-14 12:52:10 -0400

    agregamos imagenes de las vistas y agregamos contenido al template de home

--- src/lib/view/templateHome.js
+++ src/lib/view/templateHome.js
@@ -3,7 +3,33 @@
 export const home = () => {
   const divHome = document.createElement('div');
   const viewHome = `
-    <h1>Home</h1>
+  <header class="headerContent">
+  <div class="content">
+  <div class="contentTitles">
+  <img src="./image/globito.svg" alt="ubicacion" class=ubicationImg>
+  <h3 class="direction">Av. Padre Alberto Hurtado 060, Est.</h3>
+  </div>
+  <div class="titles">
+  <h4 class="sendOrder">Retira mañana desde las 9:00 am</h4>
+  </div>
+  </div>
+  <img src="./image/carrito_header.svg" alt="carrito" class="cart">
+</header>
+<main class="mainContent">
+<input type="search" placeholder="¿Que productos buscas hoy?" class="search"><img src="./image/search.svg" alt="buscar" class="searchImg">
+<img src="./image/cheque ahorro.png" alt="puntos MiClub" class="slider1">
+<img src="./image/mislistas1.png" alt="listas" class="lists">
+<h2 class="tPMCTitle">Tus productos Más Comprados</h2><h4 class="moreTitle">Ver más</h4>
+<div>
+  <!-- Contenido dinamico -->
+</div>
+</main>
+<footer class="navBar">
+  <img src="./image/home.svg" alt="">
+  <img src="./image/pasillos.svg" alt="">
+  <img src="./image/list.svg" alt="">
+  <img src="./image/profile.svg" alt="">
+</footer>
     
 `;
 
